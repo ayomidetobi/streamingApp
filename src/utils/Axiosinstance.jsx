60a83eda03aaabfb5c2f546cad47a9ae18661465
ayomidetobi <ayomidestobsy10@gmail.com>
2024-06-03 23:33:50 +0100

    onrender

--- src/utils/Axiosinstance.jsx
+++ src/utils/Axiosinstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://videostream-gvi2.onrender.com/api/videos/api',
+  baseURL: 'https://videostream-gvi2.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
